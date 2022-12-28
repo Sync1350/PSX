b3c03c24724f985050521d643b407247197db34c
Sync <103685018+Sync1350@users.noreply.github.com>
2022-12-28 20:17:15 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sync1350/PSX/main/main.lua", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Sync1350/PSX/main/helper.lua", true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Sync1350/PSX/main/bypass.lua", true))()
 
--- Loading the script and helper
+-- Loading the script and Bypass
