4148470e464a7abba0cfbba223a06021dd2efa09
Sync <103685018+Sync1350@users.noreply.github.com>
2023-01-01 15:46:43 +0000

    Create dupeload.lua

---
+++ dupeload.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Sync1350/PSX/main/bypass.lua", true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Sync1350/PSX/main/dupe.lua')))()
+
+-- loading dupe and bypass
